a1c4bf681838ff68b82b5975a072eaebaf8e7868
Atah Fatahillah <fatahillah.atah@gmail.com>
2021-12-30 15:16:07 +0700

    init

---
+++ app/src/main/java/id/umma/prayertimes/fatahillah/view/MainActivity.java
@@ -0,0 +1,16 @@
+package id.umma.prayertimes.fatahillah.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.umma.prayertimes.fatahillah.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
